9674ad138ff957fe01f9735522775206e02462a6
Pavel Naumenko <ghostblade3301@gmail.com>
2022-12-04 05:43:56 +0300

    prereview version v2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,8 @@
-from django.core.paginator import Paginator
 from django.conf import settings
-from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import PostForm
 from .models import Group, Post
--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,8 +1,7 @@
-from django.views.generic import CreateView
-
 # Функция reverse_lazy позволяет получить URL по параметрам функции path()
 # Берём, тоже пригодится
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 # Импортируем класс формы, чтобы сослаться на неё во view-классе
 from .forms import CreationForm
