d0e5d626c6e5afd0fb9afce853f80fde9a7a21e0
Pavel Naumenko <pavelnaumenkowork@gmail.com>
2023-01-26 16:23:56 +0300

    added tests for urls

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,15 +1,53 @@
 # posts/tests/test_urls.py
 from django.test import TestCase, Client
+from http import HTTPStatus
+from posts.models import User, Group, Post
 
 
-class StaticURLTests(TestCase):
-    def setUp(self):
-        # Устанавливаем данные для тестирования
-        # Создаём экземпляр клиента. Он неавторизован.
-        self.guest_client = Client()
+class TestPostsURLs(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
 
-    def test_homepage(self):
-        # Отправляем запрос через client,
-        # созданный в setUp()
-        response = self.guest_client.get('/')  
-        self.assertEqual(response.status_code, 200)
+        # create non-authorized client
+        cls.guest_client = Client()
+
+        # create authorized client
+        cls.user = User.objects.create(username='Test')
+        cls.authorized_client = Client()
+        cls.authorized_client.force_login(cls.user)
+
+        # create post in DB
+        cls.post = Post.objects.create(
+            author=cls.user,
+            id=100,
+            text='Тестовый текст',
+        )
+
+        # create group in DB
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='test-slug',
+            description='Тестовое описание группы',
+        )
+
+        # test templates and pages
+    def test_urls_uses_correct_template(self):
+        templates_url_names = {
+            '/': 'posts/index.html',
+            '/group/test-slug/': 'posts/group_list.html',
+            '/profile/Test/': 'posts/profile.html',
+            '/posts/100/': 'posts/post_detail.html',
+            '/create/': 'posts/create.html',
+            '/posts/100/edit/': 'posts/create.html',
+        }
+
+        for address, template in templates_url_names.items():
+            with self.subTest(address=address):
+                response = self.authorized_client.get(address)
+                self.assertTemplateUsed(response, template)
+
+    def test_home_url_exists_at_desired_location(self):
+        """Страница / доступна любому пользователю."""
+        response = self.guest_client.get('/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
